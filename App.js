e60b67346e8ddac431917aad3b2ad00033e76ac7
Ezer Isai Angeles <ezerangeles1@gmail.com>
2021-11-08 11:58:15 -0800

    CRUD practice
    -added creating tasks
    -added deleting tasks

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Task from './components/Task';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TextInput,
+} from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [text, onChangeText] = useState("");
+
+  const handleDelete = (id) => {
+    //const arr = tasks.filter((item) => item.id !== id);
+    const arr = [...tasks];
+    arr.splice(id, 1);
+    setTasks(arr);
+  };
+
   return (
     <View style={styles.container}>
-
       {/* Today's Tasks */}
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <Button
+          onPress={() => setTasks((oldArray) => [...oldArray, { name: text }])}
+          title="Add Task"
+        ></Button>
 
         <View style={styles.items}>
           {/* Tasks */}
-          <Task text={"Task 1"}/>
-          <Task text={"Task 2"}/>
+          {/*<Task text={"Task 1"} />*/}
+          <FlatList
+            data={tasks}
+            renderItem={({ item, index }) => (
+              <Task deleteTask={() => handleDelete(index)} text={item.name} />
+            )}
+          />
         </View>
-
       </View>
-
     </View>
   );
 }
@@ -25,7 +53,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5dc',
+    backgroundColor: "#f5f5dc",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
   tasksWrapper: {
     paddingTop: 80,
